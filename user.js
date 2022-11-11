044d5ef24710dfded4ad1b6ed8f067ba03101e83
dos-dos <83476628+dos-dos@users.noreply.github.com>
2022-11-11 13:35:29 -0800

    Add files via upload

---
+++ user.js
@@ -0,0 +1,8 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.content-properties.content.enabled", true);
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.enabled", true);
+user_pref("mousewheel.with_control.action", 5);
+user_pref("browser.urlbar.maxRichResults", 8);
+user_pref("browser.urlbar.suggest.topsites", false);
